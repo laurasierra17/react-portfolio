1c2154065475fbc6b128d8f2162a75af34a3066a
laurasierra17 <laura.sierra17@gmail.com>
2022-08-25 11:49:13 -0400

    header component created

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function Header() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') return <About />;
+        if (currentPage === 'Portfolio') return <Portfolio />;
+        if (currentPage === 'Contact') return <Contact />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            {/* Passing the current page and a functio to update it */}
+            <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {/* Calling renderPage to return a component */}
+            {renderPage()}
+        </div>
+    )
+}

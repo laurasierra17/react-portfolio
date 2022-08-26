880b57dc86951fc4fd16c78ecacd431b60827cf0
laurasierra17 <laura.sierra17@gmail.com>
2022-08-25 22:45:27 -0400

    footer created

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="https://www.linkedin.com/in/laurasierra2022"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+            <a href="https://www.github.com/laurasierra17"><i class="fa-brands fa-github fa-2xl"></i></a>
+            <a href="https://medium.com/@laura.sierra17"><i class="fa-brands fa-medium fa-2xl"></i></a>
+            <p>Designed and developed by Laura Sierra</p>
+        </footer>
+    )
+}

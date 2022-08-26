623e4e91815ded069fde41fbf5c898e31eae69c4
laurasierra17 <laura.sierra17@gmail.com>
2022-08-25 23:10:04 -0400

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer>
-            <a href="https://www.linkedin.com/in/laurasierra2022"><i class="fa-brands fa-linkedin fa-2xl"></i></a>
-            <a href="https://www.github.com/laurasierra17"><i class="fa-brands fa-github fa-2xl"></i></a>
-            <a href="https://medium.com/@laura.sierra17"><i class="fa-brands fa-medium fa-2xl"></i></a>
-            <p>Designed and developed by Laura Sierra</p>
-        </footer>
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+                    <li className="ms-3"><a href="https://www.linkedin.com/in/laurasierra2022"><i className="fa-brands fa-linkedin fa-2xl"></i></a></li>
+                    <li className="ms-3"><a href="https://www.github.com/laurasierra17"><i class="fa-brands fa-github fa-2xl"></i></a></li>
+                    <li className="ms-3"><a href="https://medium.com/@laura.sierra17"><i class="fa-brands fa-medium fa-2xl"></i></a></li>
+                </ul>
+                <div className="col-md-4 d-flex align-items-center">
+                    <span className="text-muted">Designed and developed by Laura Sierra</span>
+                </div>
+            </footer>
+        </div>
     )
 }

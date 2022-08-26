2fcc4518a1ed57755f55814cd78776d989086ddb
laurasierra17 <laura.sierra17@gmail.com>
2022-08-25 21:16:47 -0400

    form in contact page created

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
-    
+    const [formInfo, setFormInfo] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormInfo({
+            ...formInfo,
+            [name]: value
+        })
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setFormInfo({
+            name: '',
+            email: '',
+            message: ''
+        })
+    }
+
+    return (
+        <div>
+            <p>Hola pls leave a message</p>
+
+            <form>
+                <input
+                    value={formInfo.name}
+                    name="name"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Name"
+                />
+                <input
+                    value={formInfo.email}
+                    name="email"
+                    onChange={handleInputChange}
+                    type="text"
+                    placeholder="Email"
+                />
+                <input
+                    value={formInfo.message}
+                    name="message"
+                    onChange={handleInputChange}
+                    type="message"
+                    placeholder="Message"
+                />
+                <button type="button" onClick={handleFormSubmit}>Submit</button>
+            </form>
+        </div>
+    )
 }
